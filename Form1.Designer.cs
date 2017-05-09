98905302e0ae8d02657bf2fcd0b725d3407d3f77
Lavember <extremecraftgma@gmail.com>
2017-05-09 16:40:02 -0300

    code added
    hi

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Uniglitcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listAlterations = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(31, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arquivo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bungee Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(261, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nenhum selecionado!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(136, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Selecionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(792, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "GLITCHZAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listAlterations
+            // 
+            this.listAlterations.FormattingEnabled = true;
+            this.listAlterations.Location = new System.Drawing.Point(36, 96);
+            this.listAlterations.Name = "listAlterations";
+            this.listAlterations.Size = new System.Drawing.Size(851, 407);
+            this.listAlterations.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 521);
+            this.Controls.Add(this.listAlterations);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Uniglitcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listAlterations;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Uniglitcher
+{
+    public partial class Form1 : Form
+    {
+        string toGlitch;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.AddExtension = true;
+            dialog.ShowDialog();
+            dialog.DefaultExt = "uni";
+            dialog.Filter = "Unimaker (*.uni)|*.uni";
+            toGlitch = dialog.FileName;
+            if(toGlitch != "")
+            {
+                button2.Enabled = true;
+                label2.Text = toGlitch;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if(toGlitch != "")
+            {
+                listAlterations.Items.Clear();
+                string[] flines = File.ReadAllLines(toGlitch);
+                string[] nlines = new string[flines.Length];
+                for(int i = 0;i < nlines.Length; i++)
+                {
+
+                    if (i == 0)
+                    {
+                        /*
+                         * A primeira linha é basicamente o bioma do mapa
+                         * 0-23
+                         */
+
+                        int ranGen = new Random().Next(Convert.ToInt32(flines[i]), 23);
+                        nlines[i] = ranGen.ToString();
+                        listAlterations.Items.Add("Bioma do mapa mudado para " + ranGen.ToString());
+
+                    } else if (i == 1)
+                    {
+                        /*
+                         * Segunda linha é o tamanho do mapa
+                         * 0-16
+                         */
+                        int ranGen = new Random().Next(0, 16);
+                        nlines[i] = ranGen.ToString();
+                        listAlterations.Items.Add("Tamanho do mapa mudado para " + ranGen.ToString());
+
+                    } else if (i == 2)
+                    {
+                        /*
+                         * A terceira linha é o tempo
+                         * 1-9999
+                         */
+                        int ranGen = new Random().Next(1, 9999);
+                        nlines[i] = ranGen.ToString();
+                        listAlterations.Items.Add("Tempo do mapa mudado para " + ranGen.ToString());
+
+                        /*
+                         * a linha 4 n sei oq faz
+                         */
+                    } else if(i == 4)
+                    {
+                        /*
+                         * a linha 5 é o autoscroll
+                         * 0-4
+                         */
+
+                        int ranGen = new Random().Next(0, 4);
+                        nlines[i] = ranGen.ToString();
+                        listAlterations.Items.Add("Scroll do mapa mudado a " + ranGen);
+
+                    } else if (i == 5)
+                    {
+                        /*
+                         * a linha 6 é a mais intrigante
+                         * ela contém tds os blocos do mapa ajunto de posições
+                         * 
+                         */
+                        string aaa = flines[i];
+                        StringBuilder newer = new StringBuilder(aaa);
+                        
+                        for(int a = 0; a < aaa.Length; a++)
+                        {
+
+
+                            if(aaa[a] == 'F')
+                            {
+                                newer[a] = 'A';
+                                listAlterations.Items.Add("Mudado " + aaa[a] + " em " + a.ToString() + " para " + newer[a]);
+
+                            } else if (aaa[a] == 'B')
+                            {
+                                //newer[a] = 'C';
+                                //listAlterations.Items.Add("Mudado " + aaa[a] + " em " + a.ToString() + " para " + newer[a]);
+
+                            }
+                            else if (aaa[a] == 'D')
+                            {
+                                //newer[a] = 'E';
+
+                            }
+                            else if (aaa[a] == '1')
+                            {
+                                //newer[a] = '2';
+                                //listAlterations.Items.Add("Mudado " + aaa[a] + " em " + a.ToString() + " para " + newer[a]);
+
+                            }
+                            else if (aaa[a] == '3')
+                            {
+                                //newer[a] = '4';
+                                //listAlterations.Items.Add("Mudado " + aaa[a] + " em " + a.ToString() + " para " + newer[a]);
+
+                            }
+                            else if (aaa[a] == '5')
+                            {
+                                //[a] = '6';
+
+                            }
+                            else if (aaa[a] == '7')
+                            {
+                                //newer[a] = '8';
+
+                            }
+                            else if (aaa[a] == '9')
+                            {
+                                //newer[a] = '1';
+
+                            }
+
+
+                        }
+
+                        nlines[i] = newer.ToString();
+
+
+                    }
+
+
+
+
+                }
+
+
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.AddExtension = true;
+                dialog.ShowDialog();
+                dialog.DefaultExt = "uni";
+                dialog.Filter = "Unimaker (*.uni)|*.uni";
+
+                if (dialog.FileName == "")
+                {
+                    MessageBox.Show("ESCOLHA UM ARQUIVO E TENT NOVAMENT SEU BATATINHA");
+                    return;
+                }
+
+                string file = dialog.FileName;
+
+                File.WriteAllLines(file, nlines);
+
+            }
+
+
+
+        }
+    }
+}
